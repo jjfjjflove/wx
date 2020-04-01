2b6f7a8c884005543b6aad71b5a5644919a0b9f3
纪家锋 <jiafeng@weiyuhd>
2020-04-01 17:28:28 +0800

    x

--- public/index.php
+++ public/index.php
@@ -11,10 +11,10 @@
 
 // [ 应用入口文件 ]
 namespace think;
-echo __DIR__ . '/../thinkphp/base.php';
+//echo __DIR__ . '/../thinkphp/base.php';
 // 加载基础文件
-require __DIR__ . '/../thinkphp/base.php';
-echo 'test';
+require __DIR__ . '/thinkphp/base.php';
+//echo 'test';
 // 支持事先使用静态方法设置Request对象和Config对象
 
 // 执行应用并响应
